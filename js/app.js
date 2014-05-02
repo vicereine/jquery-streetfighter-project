4462ddf71be4194da4685a73898c054cf61c8bae
vicereine <lauraseabry@gmail.com>
2014-05-01 21:29:41 -0400

    Most Recent

--- js/app.js
+++ js/app.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
+  $('body').keydown(function(event) {
+  if(event.keyCode==88) {
+    $('.ryu-still').hide();
+    $('ryu-ready').hide();
+    $('ryu-throwing').hide();
+    $('ryu-cool').show();
+  }
+})
+    $('body').keyup(function(event) {
+        if (event.keyCode == 88) {
+            $('.ryu-cool').hide();
+            $('.ryu-ready').hide();
+            $('.ryu-throwing').hide();
+            $('.ryu-still').show();
+        }
+    })
   $('.ryu').mouseenter(function() {
     $('.ryu-still').hide();
     $('.ryu-ready').show();
@@ -7,6 +23,7 @@ $(document).ready(function() {
     $('.ryu-ready').hide();
     $('.ryu-still').show();
   })
+
   .mousedown(function() {
     playHadouken();
     $('.ryu-ready').hide();
@@ -26,10 +43,18 @@ $(document).ready(function() {
     $('.ryu-throwing').hide();
     $('.ryu-ready').show();
   });
-   });
+});
+
+
+
+
+  
+
+
 
 function playHadouken () {
   $('#hadouken-sound')[0].volume = 0.5;
   $('#hadouken-sound')[0].load();
   $('#hadouken-sound')[0].play();
 }
+
